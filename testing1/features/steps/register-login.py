70fb12242ad1f2f70b4054bf786294a995e75e18
Dawei Zhang <zhang991@uwindsor.ca>
2023-03-10 02:34:15 -0500

    finish acceptance testing for registration and login

--- testing1/features/steps/register-login.py
+++ testing1/features/steps/register-login.py
@@ -3,41 +3,56 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+import time
+from datetime import datetime
 
-
+now = datetime.now() # current date and time
+date_time = now.strftime("%m/%d/%Y%H%M%S")
 
 @given(u'I am a new user')
 def open_browser(context):
-    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    context.driver.implicitly_wait(5)
-    context.driver.get("http://127.0.0.1:5000/")
+    option = webdriver.ChromeOptions()
+    option.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
+    context.driver = webdriver.Chrome(chrome_options=option)
+    context.driver.implicitly_wait(15)
+    context.driver.get("http://127.0.0.1:5000/auth/register")
 
 
-@when(u'I fill in a unique username in the username field')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When I fill in a unique username in the username field')
+@when(u'I fill in a unique {username} in the username field')
+def step_impl(context,username):
+    title_field = context.driver.find_element(By.NAME, "username")
+    title_field.send_keys(date_time)
 
 
-@when(u'do the same for the email field')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When do the same for the email field')
+@when(u'do the same for the {email} field')
+def step_impl(context,email):
+    title_field = context.driver.find_element(By.NAME, "email")
+    title_field.send_keys(date_time+"testing@gmail.com")
+
+@when(u'the {password} field')
+def step_impl(context,password):
+    title_field = context.driver.find_element(By.NAME, "password")
+    title_field.send_keys(password)
 
 
-@when(u'the password field')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When the password field')
-
-
-@when(u'confirm the password')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When confirm the password')
+@when(u'confirm the {password}')
+def step_impl(context,password):
+    title_field = context.driver.find_element(By.NAME, "password2")
+    title_field.send_keys(password)
 
 
 @when(u'click the register button')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When click the register button')
+    add_button = context.driver.find_element(By.NAME, "submit")
+    add_button.click()
+    context.driver.implicitly_wait(15)
 
 
 @then(u'the account will be registered')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the account will be registered')
+    context.driver.implicitly_wait(45)
+    time.sleep(1)
+    # context.driver.get("http://127.0.0.1:5000/auth/login")
+    dump_text = context.driver.page_source
+    # print(dump_text)
+    assert ("Congratulations, you are now a registered user!" in dump_text) is True
--- testing1/features/steps/register-ui.py
+++ testing1/features/steps/register-ui.py
@@ -6,31 +6,33 @@ from selenium.webdriver.common.by import By
 
 @given(u'the user is on the register page')
 def open_browser(context):
-    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    context.driver.implicitly_wait(5)
-    context.driver.get("http://127.0.0.1:5000/")
+    option = webdriver.ChromeOptions()
+    option.add_experimental_option("excludeSwitches", ['enable-automation', 'enable-logging'])
+    context.driver = webdriver.Chrome(chrome_options=option)
+    context.driver.implicitly_wait(15)
+    context.driver.get("http://127.0.0.1:5000/auth/register")
 
 @then(u'the page should have a text field to enter the username')
 def check_username_textbox(context):
-    status = context.driver.find_element(By.NAME, "Username").is_displayed()
+    status = context.driver.find_element(By.NAME, "username").is_displayed()
     assert status is True
 
 @then(u'the page should have a text field to enter the email')
 def check_email_textbox(context):
-    status = context.driver.find_element(By.NAME, "Email").is_displayed()
+    status = context.driver.find_element(By.NAME, "email").is_displayed()
     assert status is True
 
 @then(u'the page should have a text field to enter the password')
 def check_email_textbox(context):
-    status = context.driver.find_element(By.NAME, "Password").is_displayed()
+    status = context.driver.find_element(By.NAME, "password").is_displayed()
     assert status is True
 
 @then(u'the page should have a text field to confirm the password')
 def check_email_textbox(context):
-    status = context.driver.find_element(By.NAME, "Repeat Password").is_displayed()
+    status = context.driver.find_element(By.NAME, "password2").is_displayed()
     assert status is True
 
 @then(u'the page should have a button to register')
 def check_task_add_button(context):
-    status = context.driver.find_element(By.XPATH, "//button[contains(text(),'Register')]").is_displayed()
+    status = context.driver.find_element(By.NAME, "submit").is_displayed()
     assert status is True
