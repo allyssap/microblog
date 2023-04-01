27d2033048d6bd4db180f153fccd54adb7136a79
EmperorOfBananas <acerenn01@gmail.com>
2023-03-31 23:08:25 -0400

    response.text

--- loadtest/load.py
+++ loadtest/load.py
@@ -28,7 +28,7 @@ class MicroUser(HttpUser):
         self.context.pop()
 
     def get_csrf_token(self, response):
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
         csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
         return csrf_token
 
@@ -39,15 +39,15 @@ class MicroUser(HttpUser):
                 #form = LoginForm()
                 #form.username.data = 'Test'
                 #form.username.data = 'TestPass01$'
-                print("content: ",c.get('/auth/login').content)
-                #csrf_token = self.get_csrf_token(c.get('/auth/login'))
+                #print("content: ",c.get('/auth/login').content)
+                csrf_token = self.get_csrf_token(c.get('/auth/login'))
 
-                #response = c.post(url_for('auth.login'), data={'username': 'Test', 'password': 'TestPass01$', '_csrf_token': csrf_token})
-                #if response.status_code == 200:
-                #    print(response.status_code)
-                #else:
-                #    print('Login Unsuccessful')
-                #    response.failure('failed')
+                response = c.post(url_for('auth.login'), data={'username': 'Test', 'password': 'TestPass01$', '_csrf_token': csrf_token})
+                if response.status_code == 200:
+                    print(response.status_code)
+                else:
+                    print('Login Unsuccessful')
+                    response.failure('failed')
         '''
         with self.client.post('/auth/login', data=json.dumps({'username': 'Test9', 'password': 'TestPass01$'}),
                         headers={},
