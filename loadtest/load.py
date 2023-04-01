7e1c7b5b7b8d00e2f6680e96960dbcc2b0169359
EmperorOfBananas <acerenn01@gmail.com>
2023-03-31 20:13:37 -0400

    load.py

---
+++ loadtest/load.py
@@ -0,0 +1,41 @@
+from locust import HttpUser, between, task, SequentialTaskSet
+import json
+from app.forms import LoginForm
+from app import create_app, db
+
+class MicroUser(HttpUser):
+    wait_time = between(1, 2)
+    host = "http://localhost:5000"
+
+    def on_start(self):
+        self.app = create_app()
+        self.context = self.app.app_context()
+        self.context.push()
+        db.init_app(self.app)
+        db.create_all()
+
+    def on_stop(self):
+        db.session.remove()
+        db.drop_all()
+        self.context.pop()
+
+    @task
+    def login(self):
+        with self.context:
+            form = LoginForm()
+            form.username.data = 'Test'
+            form.username.data = 'TestPass01$'
+            response = self.client.post('/auth/login', data=form.data, follow_redirects=True)
+            assert response.status_code == 200
+        '''
+        with self.client.post('/auth/login', data=json.dumps({'username': 'Test9', 'password': 'TestPass01$'}),
+                        headers={},
+                        name='Test 0',
+                        catch_response=True
+        ) as response:
+            if response.status_code is 200:
+                print(response.status_code)
+                response.success()
+            else:
+                response.failure('failed')
+                '''
