460c7dab738ed2b34f94beec1110dd4237cccc7d
EmperorOfBananas <acerenn01@gmail.com>
2023-04-02 10:57:51 -0400

    registration

--- loadtest/load.py
+++ loadtest/load.py
@@ -23,15 +23,11 @@ class MicroUser(HttpUser):
         db.init_app(self.app)
         db.create_all()
         with self.client as c:
-            #register_response = c.post('/api/users', data=json.dumps(self.data), headers={'Content-Type': 'application/json'})
-            #if register_response.status_code != 201:
-            #    print("Registration failed")
-            #else:
-            #    print("registration success")
-            user = User(username=self.data["username"], email=self.data["email"])
-            user.set_password(password=self.data["password"])
-            db.session.add(user)
-            db.session.commit()
+            register_response = c.post('/api/users', data=json.dumps(self.data), headers={'Content-Type': 'application/json'})
+            if register_response.status_code != 201:
+                print("Registration failed: ", register_response.status_code)
+            else:
+                print("registration success")
             token_response = c.post('/api/tokens', auth=(self.data["username"], self.data["password"]))
             if token_response.status_code != 200:
                 print('login failed')
