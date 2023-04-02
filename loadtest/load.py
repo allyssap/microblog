8f5c3d29a9c8b4ccbbbf72fa3ad5db656ede7c83
EmperorOfBananas <acerenn01@gmail.com>
2023-04-01 20:22:39 -0400

    db conn

--- loadtest/load.py
+++ loadtest/load.py
@@ -20,10 +20,14 @@ class MicroUser(HttpUser):
         self.client = self.app.test_client()
         self.context = self.app.test_request_context()
         self.context.push()
+        db.init_app(self.app)
+        db.create_all()
         with self.client as c:
             self.response = c.post('/api/users', data=json.dumps(self.data), headers={'Content-Type': 'application/json'})
             
     def on_stop(self):
+        db.session.remove()
+        db.drop_all()
         self.context.pop()
 
     @task
